ae9c137f93a60d0bd1ac0dbe0fba1dd54550d88e
Rodrigo Santos <rodrigom376@gmail.com>
2024-01-15 21:22:52 -0300

    teste

---
+++ backend/controllers/TesteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace backend\controllers;
+
+use yii\web\Controller;
+
+
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function index(){
+        die('aqui');
+    }
+
+}
